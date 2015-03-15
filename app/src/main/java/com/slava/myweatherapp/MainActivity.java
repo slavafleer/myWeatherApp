17199461474220cb383837fc939f45360a6b3e83
slavafleer <slavafleer@gmail.com>
2015-03-15 15:00:20 +0200

    create Forecast Url
    in new branch

--- app/src/main/java/com/slava/myweatherapp/MainActivity.java
+++ app/src/main/java/com/slava/myweatherapp/MainActivity.java
@@ -10,5 +10,13 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Creation of Forecast URL
+        String apiKey = "f330ede89d6b04c04dfed6f53bffeafb";
+        // BeerSheva coordinates
+        Double latitude = 31.2589;
+        Double longitude = 34.7997;
+        String forecastUrl = "https://api.forecast.io/forecast/"
+                + "/" + latitude + "," + longitude;
     }
 }
