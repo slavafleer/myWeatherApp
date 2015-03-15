7dba6746d46d707a45cf6e6bd236b1480968b3c4
slavafleer <slavafleer@gmail.com>
2015-03-15 15:30:50 +0200

    create OkHttp Get request

--- app/src/main/java/com/slava/myweatherapp/MainActivity.java
+++ app/src/main/java/com/slava/myweatherapp/MainActivity.java
@@ -2,11 +2,22 @@ package com.slava.myweatherapp;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    @Override
+    // TAG for Logs
+    public static final String TAG = MainActivity.class.getSimpleName();
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -17,6 +28,29 @@ public class MainActivity extends ActionBarActivity {
         Double latitude = 31.2589;
         Double longitude = 34.7997;
         String forecastUrl = "https://api.forecast.io/forecast/"
-                + "/" + latitude + "," + longitude;
+                + apiKey + "/" + latitude + "," + longitude;
+
+        // Creation of OkHttp Web request
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(forecastUrl)
+                .build();
+        Call call = client.newCall(request);
+
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+
+                if(response.isSuccessful())
+                    Log.v(TAG, response.body().string());
+                else
+                    Log.v(TAG, "There is some problem with connection to Forecast");
+            }
+        });
     }
 }
